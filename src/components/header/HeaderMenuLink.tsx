571f202380817b017f3aaac4e5bf703cef17cc2c
Bianca Santos <biancassantos89@gmail.com>
2025-01-31 14:11:31 -0300

    menu link and main padding fixed

--- src/components/header/HeaderMenuLink.tsx
+++ src/components/header/HeaderMenuLink.tsx
@@ -12,9 +12,13 @@ function HeaderMenuLink({ url, number, text }: HeaderMenuLinkProps) {
   if (!menuContext) return null;
   const { setIsOpen } = menuContext;
 
+  /* checks if we are in the homepage */
+  const isHome = url === "/space-tourism-website";
+
   return (
     <NavLink
     to={url}
+    end={isHome} /* sets absolute path to the url if we are in the home page */
     onClick={() => setIsOpen(false)}
     >
       <strong>{number}</strong> {text}
