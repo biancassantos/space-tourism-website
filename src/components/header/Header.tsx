989f9904161e35f3ef2f7ded5e706f85b3ba98b7
Bianca Santos <biancassantos89@gmail.com>
2025-02-23 13:22:33 -0300

    logo link adjusted

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <header className="max">
       <div className="header-container max">
-        <NavLink to="/space-tourism-website">
+        <NavLink to="/space-tourism-website/">
           <img src="imgs/shared/logo.svg" alt="Logo" />
         </NavLink>
         <hr />
